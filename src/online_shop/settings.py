48bbcc727421daf9d60ff4fd0206fe74f1efbaf3
ubihinon1 <ubihinon1@gmail.com>
2018-12-25 14:14:50 +0700

    [feature/Order] template folder was moved

--- src/online_shop/settings.py
+++ src/online_shop/settings.py
@@ -85,7 +85,7 @@ ROOT_URLCONF = 'online_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../web_ui/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
